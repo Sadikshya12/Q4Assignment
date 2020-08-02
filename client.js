7e8f25572b200e1e6705b8c0e76740872bbd8323
Sadikshya Dhaugoda <annexed2@gmail.com>
2020-08-02 22:23:17 +0100

    Customisation completed

--- client.js
+++ client.js
@@ -1,17 +1,25 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	var newsSource = document.getElementById("release-template").innerHTML,
 		template = Handlebars.compile(newsSource);
 
-	$.getJSON('release.json', function(json) {
+	$.getJSON('release.json', function (json) {
+		json.releases = json.releases.sort(function (a, b) {
+			// Turns your strings into dates, and then subtract them
+			// to get a value that is either negative, positive, or zero.
+			return new Date(b.date) - new Date(a.date);
+		});
+
+		json.releases = json.releases.slice(0, 3);
+
 		var data = template(json);
 		$('.news').html(data);
 	});
 
-	Handlebars.registerHelper('formatDate', function(date, format) {
+	Handlebars.registerHelper('formatDate', function (date, format) {
 		return moment(date).format(format);
 	});
 
-	Handlebars.registerHelper('truncate', function(text, num) {
-		return text.substr(0,num) + '...';
+	Handlebars.registerHelper('truncate', function (text, num) {
+		return text.substr(0, num) + '...';
 	});
 });
